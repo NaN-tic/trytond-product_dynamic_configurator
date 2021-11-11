a2d6c73967eaf101384b590537e61022d4e77880
Àngel Àlvarez <angel@nan-tic.com>
2021-11-11 11:25:57 +0100

    - move jinja fields from template to product - take care ratio in prices. - move category suppliers tab in design. - remove whitespaces on jinja fields (name) - add quotation_uom on template and use in design

--- product.py
+++ product.py
@@ -1,8 +1,15 @@
 from trytond.model import fields
 from trytond.pool import PoolMeta
+from trytond.pyson import Eval
 
 
 class Template(metaclass=PoolMeta):
     __name__ = 'product.template'
 
     configurator_template = fields.Boolean('Template for Configurator')
+    quotation_uom = fields.Many2One(
+        'product.uom', "Quotation UOM",
+        domain=[
+            ('category', '=', Eval('default_uom_category')),
+            ],
+        depends=['default_uom_category'])
