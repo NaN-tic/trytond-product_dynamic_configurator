066d3545097d1874adbdd7f973b28d71727c4d48
Àngel Àlvarez <angel@nan-tic.com>
2022-07-19 13:04:53 +0200

    add production phantom support

--- configurator.py
+++ configurator.py
@@ -863,6 +863,8 @@ class Property(DeactivableMixin, tree(separator=' / '), sequence_ordered(),
         product_bom = ProductBom()
         product_bom.product = product
         product_bom.bom = bom
+        if 'phantom' in Product._fields:
+            product.phantom = True
         if route:
             product_bom.route = route
         res_obj.append(product_bom)
