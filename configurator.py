f4f8e254fdd4385fb877a0e4345a2930a3b2cb16
Àngel Àlvarez <angel@nan-tic.com>
2022-07-26 12:09:25 +0200

    create supplier product price on variant instead on templates #060568

--- configurator.py
+++ configurator.py
@@ -630,6 +630,8 @@ class Property(DeactivableMixin, tree(separator=' / '), sequence_ordered(),
         ProductSupplier = pool.get('purchase.product_supplier')
         Price = pool.get('purchase.product_supplier.price')
         product_supplier = ProductSupplier()
+        product_supplier.template = template
+        product_supplier.product = product
         product_supplier.party = goods_supplier
         product_supplier.on_change_party()
         product_supplier.prices = ()
@@ -654,6 +656,7 @@ class Property(DeactivableMixin, tree(separator=' / '), sequence_ordered(),
 
             if cost_price:
                 price = Price()
+                price.uom = template.purchase_uom
                 price.info_unit = template.info_unit
                 qty = ((quote.quantity / qty) * bom_input.quantity)
                 price.quantity = Uom.compute_qty(self.uom, qty,
@@ -674,7 +677,7 @@ class Property(DeactivableMixin, tree(separator=' / '), sequence_ordered(),
                         price.on_change_with_info_unit_price())
                     price.info_quantity = price.on_change_with_info_quantity()
 
-        template.product_suppliers = [product_supplier]
+        product.product_suppliers = [product_supplier]
         self.update_product_values(template, design, values, created_obj)
         return {self: (bom_input, [])}
 
