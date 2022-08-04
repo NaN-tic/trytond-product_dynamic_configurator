6ad92dc88a874779ff47a68622bd0a1be102bf90
Àngel Àlvarez Serra <angel@nan-tic.com>
2022-08-04 14:09:09 +0200

    New jinja fields (#1)
    * add new jinja models to manage description fields
    
    * fix code generation

---
+++ jinja_templates.py
@@ -0,0 +1,47 @@
+from trytond.model import (Workflow, ModelView, ModelSQL,
+    DeactivableMixin, fields, sequence_ordered)
+from trytond.pool import Pool, PoolMeta
+try:
+    from jinja2 import Template as Jinja2Template
+    jinja2_loaded = True
+except ImportError:
+    jinja2_loaded = False
+
+
+
+class JinjaTemplate(ModelSQL, ModelView):
+    'Jinja Template'
+    __name__ = 'configurator.jinja_template'
+
+    name = fields.Char('Name', required=True)
+    type_= fields.Selection([('macro', 'Macro'), ('base', 'Base')], 'Type',
+        required=True)
+    macros = fields.Many2Many('jinja_template-jinja_template_macro',
+        'template', 'macro', 'Macros',
+        domain = [('type_', '=', 'macro')])
+    jinja = fields.Text('Jinja Expression')
+    full_content = fields.Function(fields.Text('Full Content'),
+        'get_full_content')
+
+
+    def get_full_content(self, name=None):
+        text = [x.jinja for x in self.macros if x]
+        text.append(self.jinja)
+        return "\n".join(text)
+
+    def render(self, record):
+        template = Jinja2Template(self.full_content, trim_blocks=True)
+        res = template.render(record)
+        if res:
+            res = res.replace('\t', '').replace('\n', '').strip()
+        return res
+
+
+class JinjaTemplateMacros(ModelSQL):
+    'Jinja template - Jinja template macros'
+    __name__ = 'jinja_template-jinja_template_macro'
+
+    template = fields.Many2One('configurator.jinja_template', 'Template',
+        domain = [('type_', '=', 'base')], required=True)
+    macro = fields.Many2One('configurator.jinja_template', 'Macro',
+        domain = [('type_', '=', 'macro')], required=True)
