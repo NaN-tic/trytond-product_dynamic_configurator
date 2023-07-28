9f012db6ac63258088c24f39f2b7d71f72d7a015
Àngel Àlvarez <angel@nan-tic.com>
2023-07-28 15:22:23 +0200

    add red inactive emoji to view inactive products

--- product.py
+++ product.py
@@ -1,7 +1,7 @@
+import html
 from trytond.model import fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval
-
 
 class Template(metaclass=PoolMeta):
     __name__ = 'product.template'
@@ -14,6 +14,11 @@ class Template(metaclass=PoolMeta):
             ],
         depends=['default_uom_category'])
 
-
 class Product(metaclass=PoolMeta):
     __name__ = 'product.product'
+
+    def get_rec_name(self, name):
+        emoji = ''
+        if not self.active:
+            emoji = '🔴'
+        return html.unescape(emoji) + " " + super(Product, self).get_rec_name(name)
