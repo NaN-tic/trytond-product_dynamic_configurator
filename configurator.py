8a5e3b081f18fa957d70e5791a2b511e7d3472fb
Raimon Esteve <raimonesteve@gmail.com>
2023-11-08 14:41:15 +0100

    Raise UserError when TypeError render expression #163301

--- configurator.py
+++ configurator.py
@@ -252,6 +252,12 @@ class Property(tree(separator=' / '), sequence_ordered(), ModelSQL, ModelView):
         template = Jinja2Template(expression, trim_blocks=True)
         try:
             res = template.render(record)
+        except TypeError as e:
+            raise UserError(gettext(
+                'product_dynamic_configurator.msg_expression_error',
+                property=self.rec_name,
+                expression=expression[:25]+('...' if len(expression) > 25 else ''),
+                invalid=str(e)))
         except Jinja2UndefinedError as e:
             raise UserError(gettext(
                 'product_dynamic_configurator.msg_expression_error',
